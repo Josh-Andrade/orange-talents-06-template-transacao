dcc1e4fe14bcbdd5d120771559bbcb642d7a8ad5
Joshua Andrade <45312862+Josh-Andrade@users.noreply.github.com>
2021-07-28 12:05:48 -0300

    criação-projeto
    commit com o projeto criado

---
+++ transacao/src/test/java/br/com/zup/ot6/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.ot6.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
