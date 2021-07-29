cc77911be16f0cc8360e72cdc316486be26e9610
Joshua Andrade <45312862+Josh-Andrade@users.noreply.github.com>
2021-07-29 12:21:41 -0300

    autenticacao-seguranca
    implementação da autenticação via keycloak

--- transacao/src/main/java/br/com/zup/ot6/transacao/security/SecurityConfigurations.java
+++ transacao/src/main/java/br/com/zup/ot6/transacao/security/SecurityConfigurations.java
@@ -5,16 +5,30 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").permitAll()
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").hasAuthority("USER")
 				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-				.anyRequest().authenticated();
+				.anyRequest().authenticated().and()
+				.oauth2ResourceServer().jwt().jwtAuthenticationConverter(getJwtAuthenticationConverter());
+	}
+
+	// configuração do claimName definido no keycloak e removendo o prefix default
+	// do JWT
+	private JwtAuthenticationConverter getJwtAuthenticationConverter() {
+		JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+		converter.setAuthoritiesClaimName("authorities");
+		converter.setAuthorityPrefix("");
+		JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+		authenticationConverter.setJwtGrantedAuthoritiesConverter(converter);
+		return authenticationConverter;
 	}
 
 }
